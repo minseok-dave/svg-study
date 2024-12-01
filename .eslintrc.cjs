3c196c60ead199809c00e19443a73bfb1d0b8257
김민석(Dave) <90183279+minseok-dave@users.noreply.github.com>
2024-12-01 21:45:40 +0900

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  extends: ['@musma/eslint-config-react'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'warn', // or "error"
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    'react/jsx-curly-brace-presence': [
+      'error',
+      {
+        props: 'never',
+        children: 'never',
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 100,
+        semi: false,
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+      },
+    ],
+  },
+}
